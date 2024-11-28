ab4836b2043f9fb55e1c2112394d33e8bcdabd5b
screengui <134831841+screengui@users.noreply.github.com>
2024-11-28 17:11:25 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,7 @@
 if game.PlaceId == 13890902087 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Math%20Boxing",true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Math%20Boxing",true))()
 elseif game.PlaceId == 10476933477 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Strong%20Ninja%20Simulator%20Modded.lua",true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Strong%20Ninja%20Simulator%20Modded.lua",true))()
 elseif game.PlaceId == 6741970382 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/zombielab.lua",true))()
-elseif game.ReplicatedStorage.:FindFirstChild("VampireStuff") ~= nil then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/mmchub.lua",true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/zombielab.lua",true))()
 end
