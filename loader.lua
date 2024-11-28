1b4a017a50d32a0ad8f9af8ddadf3d058290ba4d
screengui <134831841+screengui@users.noreply.github.com>
2024-11-28 16:39:44 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 13890902087 then
-
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Math%20Boxing",true))()
 elseif game.PlaceId == 10476933477 then
 
 elseif game.PlaceId == 6741970382 then
