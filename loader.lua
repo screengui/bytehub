db8ea4254ce034e4b45af4e6c36b5a79642808a0
screengui <134831841+screengui@users.noreply.github.com>
2024-11-28 16:41:20 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 13890902087 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Math%20Boxing",true))()
 elseif game.PlaceId == 10476933477 then
-
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Strong%20Ninja%20Simulator%20Modded.lua",true))()
 elseif game.PlaceId == 6741970382 then
 
 end
