6fe05a53050585930550bd4ba98cf0f023113511
screengui <134831841+screengui@users.noreply.github.com>
2024-11-28 17:00:06 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,11 +4,15 @@ elseif game.PlaceId == 10476933477 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Strong%20Ninja%20Simulator%20Modded.lua",true))()
 elseif game.PlaceId == 6741970382 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/zombielab.lua",true))()
-elseif game.ReplicatedStorage.AssetsMod ~= nil then
+end
+
+if game.ReplicatedStorage.AssetsMod ~= nil then
 	pcall(function()
-                        loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/minerscave.lua",true))()
-	end)
-elseif game.ReplicatedStorage.VampireStuff ~= nil then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/minerscave.lua",true))()
+		end)
+end
+	
+if game.ReplicatedStorage.VampireStuff ~= nil then
         pcall(function()
                         loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/mmchub.lua",true))()
         end)
