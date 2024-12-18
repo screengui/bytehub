70ca0c67acd66d72a3f38e21edb4e86a0db521b5
screengui <134831841+screengui@users.noreply.github.com>
2024-12-19 02:06:56 +0800

    Create realtexturepack.lua

---
+++ realtexturepack.lua
@@ -0,0 +1,5 @@
+game:GetService("ReplicatedFirst").MetaBlocks.GrassBlock.T.Texture = "http://www.roblox.com/asset/?id=137320989412134"
+game:GetService("ReplicatedFirst").MetaBlocks.GrassBlock.Bottom.Texture = "rbxassetid://95972312598754"
+game:GetService("ReplicatedFirst").MetaBlocks.GrassBlock.B.Texture = "rbxassetid://95968044461562"
+game:GetService("ReplicatedFirst").MetaBlocks.GrassBlock.L.Texture = "rbxassetid://95968044461562"
+game:GetService("ReplicatedFirst").MetaBlocks.GrassBlock.R.Texture = "rbxassetid://95968044461562"
