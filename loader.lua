bf0f2a3cfc10073ebd5a02d05fd5fa4cfb4331f1
screengui <134831841+screengui@users.noreply.github.com>
2025-01-12 03:03:20 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,4 +4,9 @@ elseif game.PlaceId == 10476933477 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Strong%20Ninja%20Simulator%20Modded.lua",true))()
 elseif game.PlaceId == 6741970382 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/zombielab.lua",true))()
+elseif game.PlaceId == 15686049105 then
+
+else
+  print("Not Supported")
+  game.Players.LocalPlayer:Kick("Game is not supported")
 end
