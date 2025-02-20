425add1c8d07b6211868f85a0c9fca579322a7fe
screengui <134831841+screengui@users.noreply.github.com>
2025-02-20 22:56:22 +0800

    Update chubloader.lua

--- chubloader.lua
+++ chubloader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 9600578307 then
-
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/PCSX.lua",true))()
 elseif game.PlaceId == 17003365237 then
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/LC.lua",true))()
 end
