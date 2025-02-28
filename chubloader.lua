27c063768df0a8b7696c0a033f7dfcc90dd9e1a7
screengui <134831841+screengui@users.noreply.github.com>
2025-02-28 18:40:48 +0800

    Update chubloader.lua

--- chubloader.lua
+++ chubloader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 9600578307 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/PCSX.lua",true))()
 elseif game.PlaceId == 17003365237 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/LC.lua",true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Clickers%20Hub/Lebron%20Clicker.lua",true))()
 end
