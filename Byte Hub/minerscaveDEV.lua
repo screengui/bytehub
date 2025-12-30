d92edfb83fc2962d8675c1f2e2224e9223e9d5b7
screengui <134831841+screengui@users.noreply.github.com>
2025-12-30 20:40:26 +0800

    Update minerscaveDEV.lua

--- Byte Hub/minerscaveDEV.lua
+++ Byte Hub/minerscaveDEV.lua
@@ -1352,51 +1352,62 @@ if not getgenv().bytehubLoaded then
     Title = "Scaffold 3x3", 
     Description = "Place blocks in a 3x3 area below you",
     Default = false,
-    Callback = function(S)
-      So = S
-      if So then
+    Callback = function(S3)
+      So3 = S3
+      if So3 then
         local M_World = require(game.Players.LocalPlayer.PlayerScripts.MainLocalScript.CWorld)
-        local M_IDs = require(game.ReplicatedStorage.AssetsMod.IDs)
-        local BlocksByName = M_IDs.ByName.Blocks
+local M_IDs = require(game.ReplicatedStorage.AssetsMod.IDs)
+local BlocksByName = M_IDs.ByName.Blocks
 
-        local dir = 1 
-        _G.CoordsChannel = game.Players.LocalPlayer.PlayerGui.HUDGui.DataFrame.Coord:GetPropertyChangedSignal("Text"):Connect(function()
-	        if game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid") and game.Players.LocalPlayer.Character.Humanoid.Health > 0 then
-		        local placeSlot = game.Players.LocalPlayer.Character.SelectedSlot.Value
-		        local Coords = game.Players.LocalPlayer.PlayerGui.HUDGui.DataFrame.Coord.Text
-		        local strDev = string.split(Coords, " ")
-		        local pl_x = tonumber(strDev[2]:sub(0, -2))
-		        local pl_y = tonumber(strDev[3]:sub(0, -2))
-		        local pl_z = tonumber(strDev[4])
-		        local realBlock
-		        if game.Players.LocalPlayer.PlayerGui.HUDGui.Inventory.Slots["Slot"..placeSlot].Slot.Display:FindFirstChild("SlotB") then
-			        for i, v in pairs(game.Players.LocalPlayer.PlayerGui.HUDGui.Inventory.Slots["Slot"..placeSlot].Slot.Display.SlotB:GetChildren()) do
-				        realBlock = v.Name
-				        local canPlaceBlock = false
-				        local block, chunk = M_World.getBlock(pl_x, pl_y-1, pl_z)
-				        if block == nil then
-					        canPlaceBlock = true
-				        else
-					        for i, v in pairs(block) do
-						        if v == 0 then
-							        canPlaceBlock = true
-							        break
-						        end
-					        end
-				        end
-				        if canPlaceBlock == true and realBlock ~= nil then
-				        	local itemblock_info = BlocksByName[realBlock]
+local dir = 1
 
-for ox = -1, 1 do
-    for oz = -1, 1 do
-        local x = pl_x + ox
-        local y = pl_y - 1
-        local z = pl_z + oz
+_G.CoordsChannel = game.Players.LocalPlayer.PlayerGui.HUDGui.DataFrame.Coord
+:GetPropertyChangedSignal("Text"):Connect(function()
 
-        local block, chunk = M_World.getBlock(x, y, z)
+    local lp = game.Players.LocalPlayer
+    local char = lp.Character
+    local hum = char and char:FindFirstChildOfClass("Humanoid")
+    if not hum or hum.Health <= 0 then return end
+
+    local placeSlot = char.SelectedSlot.Value
+    local slotGui = lp.PlayerGui.HUDGui.Inventory.Slots["Slot"..placeSlot]
+    if not slotGui or not slotGui.Slot.Display:FindFirstChild("SlotB") then return end
+
+    local realBlock
+    for _, v in pairs(slotGui.Slot.Display.SlotB:GetChildren()) do
+        realBlock = v.Name
+        break
+    end
+    if not realBlock then return end
+
+    local itemblock_info = BlocksByName[realBlock]
+    if not itemblock_info then return end
+
+    local coordText = lp.PlayerGui.HUDGui.DataFrame.Coord.Text
+    local x, y, z = coordText:match("(%-?%d+),%s*(%-?%d+),%s*(%-?%d+)")
+    if not x then return end
+
+    x = tonumber(x)
+    y = tonumber(y) - 1
+    z = tonumber(z)
+
+    -- build 3x3 positions
+    local positions = {}
+    for ox = -1, 1 do
+        for oz = -1, 1 do
+            positions[#positions + 1] = {x + ox, y, z + oz}
+        end
+    end
+
+    -- instant placement burst
+    for i = 1, #positions do
+    task.spawn(function()
+        local px, py, pz = unpack(positions[i])
+
+        local block, chunk = M_World.getBlock(px, py, pz)
         local canPlace = false
 
-        if block == nil then
+        if not block then
             canPlace = true
         else
             for _, v in pairs(block) do
@@ -1407,24 +1418,21 @@ for ox = -1, 1 do
             end
         end
 
-        if canPlace then
-            local did_place = M_World.placeBlock(x, y, z, chunk, dir, itemblock_info.id)
-            local Call, Name = game.ReplicatedStorage.GameRemotes.PlaceBlock:InvokeServer(
-                x, y, z, placeSlot, dir
-            )
+        if not canPlace then return end
 
-            if not Call then
-                chunk:change(x % 16, y, z % 16, Name)
-            end
+        -- client-side prediction
+        M_World.placeBlock(px, py, pz, chunk, dir, itemblock_info.id)
+
+        -- server call (this yields, but now isolated)
+        local ok, name = game.ReplicatedStorage.GameRemotes.PlaceBlock
+            :InvokeServer(px, py, pz, placeSlot, dir)
+
+        if not ok then
+            chunk:change(px % 16, py, pz % 16, name)
         end
-    end
-											end
-			        	end
-			        	break
-			        end
-		        end
-	        end
-        end)
+    end)
+							end
+end)
     else
         _G.CoordsChannel:Disconnect()
     end
@@ -1435,7 +1443,7 @@ for ox = -1, 1 do
     Title = "Dupe GUI",
     Description = "Loads the Dupe GUI by Argentum Exploitz",
     Callback = function()
-      loadstring(game:HttpGet("https://gist.githubusercontent.com/raw/b8d379c1e296ade8305c2fe4df652537"))()
+		loadstring(game:HttpGet("https://gist.githubusercontent.com/raw/b8d379c1e296ade8305c2fe4df652537"))()
     end
   })
   
