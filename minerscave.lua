8c5f057e59cd96d7b761cf4d6c709df78c540e0d
screengui <134831841+screengui@users.noreply.github.com>
2025-12-31 15:00:06 +0800

    Update minerscave.lua

--- minerscave.lua
+++ minerscave.lua
@@ -46,6 +46,10 @@ if not getgenv().bytehubLoaded then
     "sbjmp",
     "CraftBloxPro9999",
     "CraftTopiaIsAwesome",
+    "vconviict",
+	"minerscraftdupedemon",
+	"MinersCraftPro9999",
+	"werewareikyouseethis",
     "Epicguy_616161"
   }
 
