5f84ff3104face64e16275615e4451ab0e7d483d
screengui <134831841+screengui@users.noreply.github.com>
2026-01-11 01:13:29 +0800

    Update and rename target-strafe to target-strafe.lua

---
+++ Byte Hub/minerscave/modules/target-strafe.lua
@@ -0,0 +1,32 @@
+local EssentialsModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Byte%20Hub/minerscave/modules/Essentials.lua"))()
+
+if not _G.TargetStrafe then
+  if hbConn then
+    hbConn:Disconnect()
+    hbConn = nil
+  end
+  return
+end
+
+hbConn = RunService.Heartbeat:Connect(function(dt)
+  if not _G.TargetStrafe then return end
+  local lpChar = game.Players.LocalPlayer.Character
+  local lpHRP = lpChar and lpChar:FindFirstChild("HumanoidRootPart")
+  if not lpHRP then return end
+
+  local target = _G.selectedTargeting == "lowest" and EssentialsModule.getLowestHealthNearbyPlayer() or EssentialsModule.getClosestPlayer()
+  local tChar = target and target.Character
+  local tHRP = tChar and tChar:FindFirstChild("HumanoidRootPart")
+  if not tHRP then return end
+
+  timeAcc += dt * speed
+
+  local offset = Vector3.new(
+    math.cos(timeAcc) * _G.radius,
+    0,
+    math.sin(timeAcc) * _G.radius
+  )
+
+  local targetPos = tHRP.Position
+  lpHRP.CFrame = CFrame.new(targetPos + offset, targetPos)
+end)
