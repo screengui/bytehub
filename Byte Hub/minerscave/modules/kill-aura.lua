37268b043a14c1002473218a5e3030840a8b41e2
screengui <134831841+screengui@users.noreply.github.com>
2026-01-10 23:20:03 +0800

    Update kill-aura.lua

--- Byte Hub/minerscave/modules/kill-aura.lua
+++ Byte Hub/minerscave/modules/kill-aura.lua
@@ -1,1 +1,28 @@
+local Attack = game.ReplicatedStorage.GameRemotes.Attack
 
+local function attackLoop()
+		while _G.KillAura do
+		    local lpChar = game.Players.LocalPlayer.Character
+        local lpHRP = lpChar and lpChar:FindFirstChild("HumanoidRootPart")
+        
+        if lpHRP then
+            local target = selectedTargeting == "lowest" and getLowestHealthNearbyPlayer() or getClosestPlayer()
+
+            if target and target.Character then
+                local tHRP = target.Character:FindFirstChild("HumanoidRootPart")
+                if tHRP then
+                    local d = lpHRP.Position - tHRP.Position
+                    if (d.X*d.X + d.Z*d.Z) <= RANGE_SQ then
+                        Attack:InvokeServer(target.Character)
+                    end
+                end
+            end
+        end
+		    task.wait(delay)
+		 end
+  end
+
+	if ka then
+		  task.spawn(attackLoop)
+  end
+end
