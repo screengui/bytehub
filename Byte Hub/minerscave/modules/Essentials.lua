bbab4a54af5dfc61925ea817195d9bb8ad672edc
screengui <134831841+screengui@users.noreply.github.com>
2026-01-11 00:01:58 +0800

    Update Essentials.lua

--- Byte Hub/minerscave/modules/Essentials.lua
+++ Byte Hub/minerscave/modules/Essentials.lua
@@ -1,1 +1,60 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixeluted/adoniscries/refs/heads/main/Source.lua",true))()
+wait()
+-- Anti Kick --
+  
+local oldhmmi
+local oldhmmnc
+oldhmmi = hookmetamethod(game, "__index", function(self, method)
+  if self == player and method:lower() == "kick" then
+    return error("Expected ':' not '.' calling member function Kick", 2)
+  end
+  return oldhmmi(self, method)
+end)
+oldhmmnc = hookmetamethod(game, "__namecall", function(self, ...)
+  if self == player and getnamecallmethod():lower() == "kick" then
+    return
+  end
+  return oldhmmnc(self, ...)
+end)
 
+local function getLowestHealthNearbyPlayer()
+  local lowestHealth = math.huge
+  local targetPlayer = nil
+    
+  local localHRP = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+  if not localHRP then return nil end
+    
+  for _, player in ipairs(game.Players:GetPlayers()) do
+    if player ~= LP and player.Character then
+      local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+      local targetHRP = player.Character:FindFirstChild("HumanoidRootPart")
+      if humanoid and targetHRP and humanoid.Health > 0 then
+        local distance = (targetHRP.Position - localHRP.Position).Magnitude
+        if distance <= strafeRange and humanoid.Health < lowestHealth then
+          lowestHealth = humanoid.Health
+          targetPlayer = player
+        end
+      end
+    end
+  end
+    
+  return targetPlayer
+end
+
+local function getClosestPlayer()
+  local closest = nil
+  local shortest = math.huge
+  local hrp = game.Players.LocalPlayer.Character and LP.Character:FindFirstChild("HumanoidRootPart")
+  if not hrp then return nil end
+
+  for _, player in ipairs(Players:GetPlayers()) do
+    if player ~= LP and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+      local dist = (hrp.Position - player.Character.HumanoidRootPart.Position).Magnitude
+      if dist < shortest then
+        shortest = dist
+        closest = player
+      end
+    end
+  end
+  return closest
+end
