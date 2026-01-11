4b9f69525b9da1b97d5658e89760ef074a7f0188
screengui <134831841+screengui@users.noreply.github.com>
2026-01-11 12:05:36 +0800

    Update kill-aura.lua

--- Byte Hub/minerscave/modules/kill-aura.lua
+++ Byte Hub/minerscave/modules/kill-aura.lua
@@ -1,29 +1,46 @@
+local KillAura = {}
+local running = false
+
 local Attack = game.ReplicatedStorage.GameRemotes.Attack
+local Players = game:GetService("Players")
+local LP = Players.LocalPlayer
 
-local EssentialsModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Byte%20Hub/minerscave/modules/Essentials.lua"))()
+local Essentials = loadstring(game:HttpGet(
+  "https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Byte%20Hub/minerscave/modules/Essentials.lua"
+))()
 
-local function attackLoop()
-	while _G.KillAura do
-		local lpChar = game.Players.LocalPlayer.Character
-        local lpHRP = lpChar and lpChar:FindFirstChild("HumanoidRootPart")
-        
-        if lpHRP then
-            local target = _G.selectedTargeting == "lowest" and EssentialsModule.getLowestHealthNearbyPlayer() or EssentialsModule.getClosestPlayer()
+local function loop()
+	while running do
+		local char = LP.Character
+		local hrp = char and char:FindFirstChild("HumanoidRootPart")
+		if hrp then
+			local target =
+				_G.selectedTargeting == "lowest"
+				and Essentials.getLowestHealthNearbyPlayer()
+				or Essentials.getClosestPlayer()
 
-            if target and target.Character then
-                local tHRP = target.Character:FindFirstChild("HumanoidRootPart")
-                if tHRP then
-                    local d = lpHRP.Position - tHRP.Position
-                    if (d.X*d.X + d.Z*d.Z) <= _G.RANGE_SQ then
-                        Attack:InvokeServer(target.Character)
-                    end
-                end
-            end
-        end
+			if target and target.Character then
+				local tHRP = target.Character:FindFirstChild("HumanoidRootPart")
+				if tHRP then
+					local d = hrp.Position - tHRP.Position
+					if (d.X*d.X + d.Z*d.Z) <= _G.RANGE_SQ then
+						Attack:InvokeServer(target.Character)
+					end
+				end
+			end
+		end
 		task.wait(_G.delay)
 	end
 end
 
-if _G.KillAura then
-    task.spawn(attackLoop)
+function KillAura.start()
+	if running then return end
+	running = true
+	task.spawn(loop)
 end
+
+function KillAura.stop()
+	running = false
+end
+
+return KillAura
