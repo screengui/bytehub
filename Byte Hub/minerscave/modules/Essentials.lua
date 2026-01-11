53268bb8de6f3d9b735854d84af946fb80cef248
screengui <134831841+screengui@users.noreply.github.com>
2026-01-11 11:45:37 +0800

    Update Essentials.lua

--- Byte Hub/minerscave/modules/Essentials.lua
+++ Byte Hub/minerscave/modules/Essentials.lua
@@ -1,20 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Byte%20Hub/minerscave/modules/globals.lua",true))()
--- Anti Kick --
-  
-local oldhmmi
-local oldhmmnc
-oldhmmi = hookmetamethod(game, "__index", function(self, method)
-  if self == player and method:lower() == "kick" then
-    return error("Expected ':' not '.' calling member function Kick", 2)
-  end
-  return oldhmmi(self, method)
-end)
-oldhmmnc = hookmetamethod(game, "__namecall", function(self, ...)
-  if self == player and getnamecallmethod():lower() == "kick" then
-    return
-  end
-  return oldhmmnc(self, ...)
-end)
 
 local function getLowestHealthNearbyPlayer()
   local lowestHealth = math.huge
