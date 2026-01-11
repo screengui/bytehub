5bac92a1a0b4172de17456b1ffa8314590850992
screengui <134831841+screengui@users.noreply.github.com>
2026-01-11 08:19:56 +0800

    Update hitbox-expander.lua

--- Byte Hub/minerscave/modules/hitbox-expander.lua
+++ Byte Hub/minerscave/modules/hitbox-expander.lua
@@ -20,10 +20,9 @@ if _G.HitboxExpander then
     end)
 else
     if connection then connection:Disconnect() end
-        for _, player in ipairs(game.Players:GetPlayers()) do
-            if player ~= game.Players.LocalPlayer then
-                changeTorsoSize(player, Vector3.new(2, 2, 1), true, 0)
-            end
+    for _, player in ipairs(game.Players:GetPlayers()) do
+        if player ~= game.Players.LocalPlayer then
+            changeTorsoSize(player, Vector3.new(2, 2, 1), true, 0)
         end
     end
 end
