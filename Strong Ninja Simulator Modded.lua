8e92278c0508f0bd44aab91d04261ebe9b0f092f
screengui <134831841+screengui@users.noreply.github.com>
2026-08-22 20:28:20 +0800

    Update Strong Ninja Simulator Modded.lua

--- Strong Ninja Simulator Modded.lua
+++ Strong Ninja Simulator Modded.lua
@@ -1,1 +1,2 @@
-
+setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/screengui/bytehub/refs/heads/main/Byte%20Hub/Strong_Ninja_Simulator_Modded.lua"))()')
+game.Players.LocalPlayer:Kick("Old Loadstring Detected!\nNew Loadstring copied to clipboard.")
