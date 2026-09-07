bfeccb39d24fcd1c7f5e34863be64508f4c3bf3c
screengui <134831841+screengui@users.noreply.github.com>
2026-09-08 07:36:57 +0800

    Update minerscaveDEV.lua

--- Byte Hub/minerscaveDEV.lua
+++ Byte Hub/minerscaveDEV.lua
@@ -11,7 +11,7 @@ if getgenv().bytehubLoaded then
 end
 
 getgenv().bytehubLoaded = true
-local version = "pre-release v4.6.1"
+local version = "pre-release v4.6.4"
 -- Services --
 local TweenService = game:GetService("TweenService")
 local UserInputService = game:GetService("UserInputService")
